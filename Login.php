73c876c436bbc576a805a03c138cfaee7836d6dc
Jest <jeremiah.lirag@gmail.com>
2025-07-09 22:26:52 +0800

    Landing-Page [FIXES]
    Get Started Redirect [FEATURE]

--- Login.php
+++ Login.php
@@ -28,10 +28,10 @@ try {
 </head>
 <body>
 
-    <header style="">
+    <header>
         <nav class="adminnav" style="padding-top: 20px; padding-bottom: 20px; background: #ffffff;
 background: linear-gradient(180deg,rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 1) 51%, rgba(255, 255, 255, 0.58) 100%); color: var(--background-color); width: auto; text-align: center;">
-            <h1>JMCYK Client & Receipts Management System</h1>
+            <h1>BookSync Management System</h1>
         </nav>
     </header>
 
